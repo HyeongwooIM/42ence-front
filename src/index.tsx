74a9a1055e94a479af783ed7883dc3dd4835d081
HyeongwooIM <hyeongwoo1279@gmail.com>
2023-06-19 20:29:16 +0900

    123

--- src/index.tsx
+++ src/index.tsx
@@ -12,10 +12,6 @@ import ErrorChecker from "./components/Error/ErrorChecker";
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
-if (window.location.protocol === 'https:' && process.env.NODE_ENV === 'development') {
-  // 개발 환경에서만 HTTP 요청을 허용
-  // 리액트 개발 서버에 프록시 설정을 통해 HTTPS로 전달되는 요청을 HTTP로 변경하는 방법이 있음
-  process.env.REACT_APP_API_BASE_URL = 'http://api.example.com';}
 root.render(
   // <React.StrictMode>
   <RecoilRoot>
