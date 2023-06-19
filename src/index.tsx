0e01bbbcb01db718aa22b9ec0d5ca48cca62d6e6
HyeongwooIM <hyeongwoo1279@gmail.com>
2023-06-19 20:08:08 +0900

    123

--- src/index.tsx
+++ src/index.tsx
@@ -8,9 +8,14 @@ import { RecoilRoot } from "recoil";
 
 import ErrorChecker from "./components/Error/ErrorChecker";
 
+
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
+if (window.location.protocol === 'https:' && process.env.NODE_ENV === 'development') {
+  // 개발 환경에서만 HTTP 요청을 허용
+  // 리액트 개발 서버에 프록시 설정을 통해 HTTPS로 전달되는 요청을 HTTP로 변경하는 방법이 있음
+  process.env.REACT_APP_API_BASE_URL = `${process.env.REACT_APP_END_POINT}`;
 root.render(
   // <React.StrictMode>
   <RecoilRoot>
